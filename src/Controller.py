e5759ab7811bec55f8a68e1e9ee1c1302b9e514b
Jeremy A. Kolbe <J.KOLBE2@labs.clark.edu>
2019-06-10 12:29:08 -0700

    added notes

--- src/Controller.py
+++ src/Controller.py
@@ -5,6 +5,18 @@
 import View
 import Model
 
+'''
+notes: 
+
+v = view()
+m = model(v)
+self.player = "x"
+v.message("x's turn")
+while TRUE:
+    cn=v.getmouse()
+    
+'''
+
 class Controller:
 
     def __init__(self):
--- src/View.py
+++ src/View.py
@@ -1,24 +1,31 @@
 # View.py
 # 
 # For TicTacToe
+'''
+notes:
+
+self.win=graph.win(parameters)
+def draw o.(self, cellnum)
+circ = circle()
+circ.draw(self.win)
+
+v = view()
+v.drawo(cellnum) #this calls the function that draws an o 
+
+cellnum = int(point.gety()) * 3 + int (point.getx())
+
+for errors if getx( < 3 and get y( < 3))
+'''
 
 from graphics import *
 
 class View:
 
     def __init__(self):
-        # delete and enter your code here
-        '''
-        self.win=graph.win(parameters)
-        def draw o.(self, cellnum)
-        circ = circle()
-        circ.draw(self.win)
+        win=GraphWin("Tic Tac Toe", 3, 3)
+        draw.win(self)
+          
 
-        v = view()
-        v.drawo(cellnum) #this calls the function that draws an o 
-
-        '''
-        pass
 
 def ViewTest():
     # delete and enter your code here
